00cff89fb6ae0672ce3a3d11d49e202100597db2
SilverShadow123 <hasanmohasin852@gmail.com>
2025-07-28 21:19:11 +0600

    Implement network services API with error handling and base API structure

--- lib/clean_code/config/data/exceptions/app_exceptions.dart
+++ lib/clean_code/config/data/exceptions/app_exceptions.dart
@@ -1,7 +1,7 @@
-class AppException implements Exception{
-
+class AppException implements Exception {
   final _message;
   final _prefix;
+
   AppException([this._message, this._prefix]);
 
   @override
@@ -10,24 +10,29 @@ class AppException implements Exception{
   }
 }
 
-class NoInternetException extends AppException{
-  NoInternetException([String? message] ): super(message, "No Internet Connection");
+class NoInternetException extends AppException {
+  NoInternetException([String? message])
+    : super(message, "No Internet Connection");
 }
 
-class UnauthorizedException extends AppException{
-  UnauthorizedException([String? message] ): super(message, "Unauthorized");
-}
-class RequestTimeoutException extends AppException{
-  RequestTimeoutException([String? message] ): super(message, "Request Timeout");
-}
-class FetchDataException extends AppException{
-  FetchDataException([String? message] ): super(message, "Error During Communication");
+class UnauthorizedException extends AppException {
+  UnauthorizedException([String? message]) : super(message, "Unauthorized");
 }
 
-class BadRequestException extends AppException{
-  BadRequestException([String? message] ): super(message, "Invalid Request");
+class RequestTimeoutException extends AppException {
+  RequestTimeoutException([String? message])
+    : super(message, "Request Timeout");
 }
 
-class ConflictException extends AppException{
-  ConflictException([String? message] ): super(message, "Conflict");
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+    : super(message, "Error During Communication");
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, "Invalid Request");
+}
+
+class ConflictException extends AppException {
+  ConflictException([String? message]) : super(message, "Conflict");
 }
---
+++ lib/clean_code/config/data/network/network_services_api.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bloc_learning/clean_code/config/data/exceptions/app_exceptions.dart';
+import 'package:bloc_learning/clean_code/config/data/network/base_api_services.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkServicesApi implements BaseApiServices{
+  @override
+  Future<dynamic> getApi(String url) async{
+
+    dynamic jsonResponse;
+
+   try{
+     
+     final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+     jsonResponse = returnResponse(response);
+
+
+     if(response.statusCode == 200){
+
+     }
+   }on SocketException{
+     throw NoInternetException('');
+   }on TimeoutException{
+      throw TimeoutException('Request timeout');
+   }
+   return jsonResponse;
+  }
+
+  @override
+  Future<dynamic> postApi(String url,var data) async{
+    dynamic jsonResponse;
+
+    try{
+
+      final response = await http.post(Uri.parse(url), body: data).timeout(const Duration(seconds: 10));
+      jsonResponse = returnResponse(response);
+
+
+      if(response.statusCode == 200){
+
+      }
+    }on SocketException{
+      throw NoInternetException('');
+    }on TimeoutException{
+      throw TimeoutException('Request timeout');
+    }
+    return jsonResponse;
+  }
+
+
+  dynamic returnResponse(http.Response response){
+    switch (response){
+      case 200:
+        dynamic jsonResponse = jsonDecode(response.body);
+        return jsonResponse;
+      case 400:
+        dynamic jsonResponse = jsonDecode(response.body);
+        return jsonResponse;
+      case 401:
+        throw UnauthorizedException('Unauthorized request');
+      case 500:
+       throw FetchDataException('Error occurred while communicating with server'+response.statusCode.toString());
+       default:
+         throw UnauthorizedException();
+    }
+  }
+  
+}
