dc62469473fe12164d4e8e59595f1b4b9a39b47f
SilverShadow123 <hasanmohasin852@gmail.com>
2025-08-22 19:33:22 +0600

    Add ApiResponse and MoviesModel classes for API handling and data modeling

---
+++ lib/clean_code/models/movies/movies.dart
@@ -0,0 +1,37 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'movies.freezed.dart';
+
+part 'movies.g.dart';
+
+@freezed
+sealed class MoviesModel with _$MoviesModel {
+  factory MoviesModel(
+          {@Default(0) int total,
+          @Default(0) int page,
+          @Default(0) int pages,
+          @Default([]) @JsonKey(name: 'tv_shows') List<TvShows> tvShow}) =
+      _MoviesModel;
+
+  factory MoviesModel.fromJson(Map<String, dynamic> json) =>
+      _$MoviesModelFromJson(json);
+}
+
+@freezed
+sealed class TvShows with _$TvShows {
+  factory TvShows({
+    @JsonKey(name: 'name') @Default('') String name,
+    @JsonKey(name: 'permalink') @Default('') String permalink,
+    @JsonKey(name: 'start_date') @Default('') String startDate,
+    @JsonKey(name: 'end_date') @Default('') String? endDate,
+    @JsonKey(name: 'country') @Default('') String country,
+    @JsonKey(name: 'network') @Default('') String network,
+    @JsonKey(name: 'status') @Default('') String status,
+    @JsonKey(name: 'image_thumbnail_path')
+    @Default('')
+    String imageThumbnailPath,
+  }) = _TvShows;
+
+  factory TvShows.fromJson(Map<String, dynamic> json) =>
+      _$TvShowsFromJson(json);
+}
--- lib/clean_code/views/login/login_screen.dart
+++ lib/clean_code/views/login/login_screen.dart
@@ -1,6 +1,5 @@
 
 import 'package:bloc_learning/clean_code/models/user/user_model.dart';
-import 'package:bloc_learning/clean_code/repository/auth/login_repository.dart';
 import 'package:bloc_learning/clean_code/views/login/widgets/widget.dart';
 import 'package:bloc_learning/main.dart';
 import 'package:flutter/material.dart';
